57a510dd485b5326aabb0d260d212dce8ec95a23
vitali <vitali.pe@gmail.com>
2015-05-30 16:00:55 -0700

    add initialize() args to Class

--- src/Class.js
+++ src/Class.js
@@ -2,7 +2,7 @@ var _extend = require("./_extend");
 
 var Class = function() {
     if (this.initialize)
-        this.initialize();
+        this.initialize.apply(this, arguments);
 };
 
 Class.extend = _extend;
--- test/_Class.js
+++ test/_Class.js
@@ -29,3 +29,13 @@ test("should invoke initialize() on instantiation", function() {
 
     assert.called(spy);
 });
+
+
+test("should be possible to pass args to initialize()", function() {
+    var spy = sinon.spy();
+    var Child = Class.extend({  initialize : spy });
+
+    new Child(1,2,3,4);
+
+    assert.calledWithExactly(spy, 1,2,3,4);
+});
