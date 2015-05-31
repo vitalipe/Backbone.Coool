6549e933e4e428501af2490367f2ebb3719e1a16
vitali <vitali.pe@gmail.com>
2015-05-31 06:38:59 -0700

    import most of the "static" trait functionality & tests

--- src/_extend.js
+++ src/_extend.js
@@ -1,4 +1,25 @@
 
 module.exports = function(spec, staticSpec) {
+    var self = this;
+    var invokeExtend = function(extend){extend.call(self, spec, staticSpec);};
+    var traits = (spec.traits || []).map(_.clone);
+    var staticTraits = _.chain(traits).pluck("static").compact().value();
+    var parentExtendTraits = (this.prototype.__extendMixins || []);
+    var extendTraits = _.chain(staticTraits).pluck("extend").compact().value();
+
+    staticSpec = (staticSpec || {});
+
+    // apply static traits
+    staticTraits.unshift({});
+    staticProps = _.extend( staticSpec, _.extend.apply(_, staticTraits));
+    delete staticProps.extend;
+
+    // apply extend for each trait & parent trait
+    _.each(parentExtendTraits, invokeExtend);
+    _.each(extendTraits, invokeExtend);
+
+    spec.__extendMixins = _.union(parentExtendTraits, extendTraits);
+
+
     return  Backbone.Model.extend.call(this, spec, staticSpec);
 };
