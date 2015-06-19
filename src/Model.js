e9f7b7bf0fff17c0eae5e25deac5a250e54f3cd2
vitalipe <vitali.pe@gmail.com>
2015-06-19 12:31:25 +0300

    import logic from CustomAttrs mixin to Model

--- src/Model.js
+++ src/Model.js
@@ -1,8 +1,104 @@
-
 var _extend = require("./extend");
+var Attribute = require("./Attribute");
+var util = require("./_util");
 
-var Model = Backbone.Model.extend({});
-Model.extend = _extend;
+var BaseModel = Backbone.Model.extend({});
+BaseModel.extend = _extend;
+
+var isAttribute = function(Attr) {
+    return util.isAncestor(Attribute, Attr);
+};
+
+var __extend = {
+    static : {
+        extend : function(spec) {
+
+            var defaults = _(spec).result("defaults");
+
+            // take custom attr classes from parent
+            spec.__customAttrClasses = _.extend({}, this.prototype.__customAttrClasses);
+
+            _.each(defaults, function(Attr, name) {
+                if (!isAttribute(Attr))
+                    return;
+
+                spec.__customAttrClasses[name] = Attr;
+                defaults[name] = Attr.defaultValue();
+            });
+
+            spec.defaults = defaults;
+        }
+    }
+};
+
+module.exports = BaseModel.extend({
+
+    traits : [__extend],
+
+    initialize : function() {
+        this.__customAttrs = {};
+
+        _(this.__customAttrClasses).each(function (Attr, name) {
+            this.__customAttrs[name] = new Attr(this, name);
+        }, this);
+    },
+
+    get : function(key) {
+        var customAttr = this.__customAttrs[key];
+
+        if (customAttr)
+            return customAttr.get();
+        else
+            return this._super(key);
+    },
+
+    set : function(key, val, options) {
+        var attrs = {};
+        var self = this;
+
+        // normalize args
+        if (_.isObject(key)) {
+            attrs = _.clone(key);
+            options = val;
+        }
+        else {
+            attrs[key] = val;
+        }
+
+        // handle custom attributes
+        _.chain(this.__customAttrs).pick(_.keys(attrs)).each(function(attr, name) {
+            attr.set(attrs[name], options);
+            self.attributes[name] = attrs[name];
+            delete attrs[name];
+        });
+
+        // now delegate normal
+        return this._super.call(this, attrs, options);
+    },
+
+    parse : function(rawData) {
+
+        var data = this._super.call(this, rawData);
+
+        _.each(this.__customAttrs, function(attr, name) {
+            if (_.has(rawData, name))
+                data[name] = attr.parse(rawData[name]);
+        });
+
+        return data;
+    },
 
 
-module.exports = Model;
+    toJSON : function(attrs) {
+        var json = this._super.call(this, attrs);
+
+        attrs = (attrs || {});
+
+        _.each(this.__customAttrs, function(attr, name) {
+            json[name] = attr.toJSON(attrs[name]);
+        });
+
+        return json;
+    }
+
+});
--- test/_Model.js
+++ test/_Model.js
@@ -1,4 +1,63 @@
 var Model = Coool.Model;
+var Attribute = Coool.Attribute;
+
+
+var createFakeAttribute = function(spec) {
+    var proxy = function() {return this._super.apply(this, arguments)};
+
+    spec = (spec || {});
+
+    var proxyFactory = function() {
+        meta.instances.push(this);
+        return this._super.apply(this, arguments)
+    };
+
+    var meta = {
+        instances : []
+    };
+
+    var staticMethods = {
+
+        getInstance : function(model, name) {
+            return _.find(meta.instances, function(a) { return a.model === model && a.name === name});
+        },
+
+        verifyItExistsOn : function(model, name) {
+            var attr = this.getInstance(model, name);
+
+            model.set(name, "test value");
+
+            assert.isDefined(attr);
+
+            if (_.isUndefined(attr))
+                return; // don't run other tests...
+
+            assert.equal(model.get(name), "test value");
+            assert.equal(attr.get(), "test value");
+        },
+
+        reset : function() {
+            _.each(methods, function(spy) { if (spy && spy.reset) spy.reset(); });
+        }
+    };
+
+    var methods = {
+        constructor : sinon.spy(proxyFactory),
+        default : spec.default,
+        initialize : sinon.spy(spec.initialize || proxy),
+        set : sinon.spy(spec.set || proxy),
+        get : sinon.spy(spec.get || proxy),
+        parse : sinon.spy(spec.parse || proxy),
+        toJSON : sinon.spy(spec.toJSON || proxy)
+    };
+
+    return Attribute.extend(_.clone(methods), _.extend(staticMethods, {methods : methods, meta : meta}));
+};
+
+var createFakeHost  = function(defaults) {
+    var Host = Model.extend({ defaults : defaults});
+    return new Host;
+};
 
 
 QUnit.module("Model");
@@ -11,4 +70,148 @@ test("should be an instance of Backbone.Model", function() {
 
 test("should expose Coool.extend()", function() {
     assert.equal(Model.extend, Coool.extend);
+});
+
+
+
+QUnit.module("Model->Custom Attributes");
+
+test("should delegate to custom getters/setters", function() {
+    var Fake = createFakeAttribute();
+    var model = createFakeHost({ myCustom : Fake });
+
+    Fake.reset();
+
+    model.get("myCustom");
+    model.set("myCustom", 12345);
+
+    assert.called(Fake.methods.get);
+    assert.calledWith(Fake.methods.set, 12345);
+});
+
+
+test("should inherent custom attrs from parent  ", function() {
+    var Fake = createFakeAttribute({ default : 42});
+    var ParentModel = Model.extend({ defaults : { myCustom : Fake, other : Fake}});
+    var ChildModel = ParentModel.extend({});
+    var child = new ChildModel();
+
+
+    Fake.verifyItExistsOn(child, "myCustom");
+    Fake.verifyItExistsOn(child, "other");
+});
+
+
+test("should be instantiated with model & name", function() {
+    var Fake = createFakeAttribute();
+    var model = createFakeHost({ myCustom : Fake });
+
+    assert.calledWith(Fake.methods.initialize, model, "myCustom");
+});
+
+
+test("should be possible to instantiate multiple times in the same object", function() {
+    var Fake = createFakeAttribute();
+    var model = createFakeHost({ fakeOne : Fake, fakeTwo : Fake });
+
+    assert.calledWithExactly(Fake.methods.initialize, model, "fakeTwo");
+    assert.calledWithExactly(Fake.methods.initialize, model, "fakeOne");
+});
+
+
+test("should pass options to Attribute.set() ", function() {
+    var options = { silent : true, someOtherOption : false};
+    var Fake = createFakeAttribute();
+    var model = createFakeHost({ myCustom : Fake});
+
+    model.set("myCustom", 42, options);
+
+    assert.calledWithExactly(Fake.methods.set, 42, options);
+});
+
+
+test("should not effect simple attributes", function() {
+    var changeEventSpy = sinon.spy();
+    var model = createFakeHost({ "myCustom" : createFakeAttribute(), simple : 42});
+    var initialValue = model.get("simple");
+
+    model.on("change change:simple", changeEventSpy);
+    model.set("simple", 12345);
+
+    assert.equal(initialValue, 42);
+    assert.equal(model.get("simple"), 12345);
+    assert.calledTwice(changeEventSpy);
+});
+
+
+test("should not store Attribute definitions in defaults, only default values", function() {
+    var Fake = createFakeAttribute({ default : "bah!"});
+    var Host = Model.extend({ defaults :{ myCustom : Fake }});
+
+    assert.equal(Host.prototype.defaults.myCustom, "bah!");
+});
+
+
+test("toJSON() should work with custom attributes", function() {
+    var FakeAttrOne = createFakeAttribute({ toJSON : sinon.stub().returns("ONE!")});
+    var FakeAttrTwo = createFakeAttribute({ toJSON : sinon.stub().returns("TWO!")});
+    var host = createFakeHost({ "one" : FakeAttrOne, "two" : FakeAttrTwo, "three" : 3});
+
+    assert.deepEqual(host.toJSON(), {
+        one : "ONE!",
+        two : "TWO!",
+        three : 3
+    });
+});
+
+
+test("parse() should work with custom attributes", function() {
+    var FakeAttrOne = createFakeAttribute({ parse : sinon.stub().returns("ONE!")});
+    var FakeAttrTwo = createFakeAttribute({ parse : sinon.stub().returns("TWO!")});
+    var host = createFakeHost({ "one" : FakeAttrOne, "two" : FakeAttrTwo, "three" : 3});
+
+    assert.deepEqual(host.parse({one : 1, two: 2, three : 3}), {
+        one : "ONE!",
+        two : "TWO!",
+        three : 3
+    });
+});
+
+
+test("should not invoke Attribute.parse() if no attribute data was passed", function() {
+    var FakeAttrOne = createFakeAttribute();
+    var FakeAttrTwo = createFakeAttribute();
+    var host = createFakeHost({ "one" : FakeAttrOne, "two" : FakeAttrTwo, "three" : 3});
+
+    host.parse({nonExistent : "moo!!", three : "three" });
+
+    assert.notCalled(FakeAttrOne.methods.parse);
+    assert.notCalled(FakeAttrTwo.methods.parse);
+});
+
+
+test("methods should be invoked with attribute as context", function() {
+    var Fake = createFakeAttribute();
+    var host = createFakeHost({ "one" : Fake});
+    var attr = Fake.getInstance(host, "one");
+
+    host.parse({ one : 11});
+    host.set("one", 1);
+    host.get("one");
+    host.toJSON();
+
+    assert.alwaysCalledOn(Fake.methods.parse, attr);
+    assert.alwaysCalledOn(Fake.methods.set, attr);
+    assert.alwaysCalledOn(Fake.methods.get, attr);
+    assert.alwaysCalledOn(Fake.methods.toJSON, attr);
+});
+
+test("set() should not alter param object", function() {
+    var host = createFakeHost({ "one" : createFakeAttribute(), simple : "yes"});
+    var params = { one : 11, simple : "no"};
+    var cloned = _.clone(params);
+
+    host.set(params);
+
+    assert.deepEqual(params, cloned);
 });
